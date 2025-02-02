c83c0d6b9633050a7df88050df1cf345579024c9
ANIMESH DUTTA <132822834+animesh65432@users.noreply.github.com>
2025-02-02 07:01:16 +0000

    fix the reducer

--- client/src/Reducers/PeerReducer.ts
+++ client/src/Reducers/PeerReducer.ts
@@ -8,6 +8,7 @@ export const peerReducer = (state: PeerState, action: PeerAction) => {
             return {
                 ...state,
                 [action.payload.roomId]: {
+                    ...state[action.payload.roomId], // Keep existing peers
                     [action.payload.peerId]: { stream: action.payload.stream }
                 }
             };
